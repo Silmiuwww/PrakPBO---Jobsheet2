93b2b68ed47b19791e5a4f6451503824757d5cde
Silmiuww <nadiasalma078@gmail.com>
2025-09-05 09:36:05 +0700

    -	Instansiasi 2 objek mahasiswa

--- MahasiswaDemo.java
+++ MahasiswaDemo.java
@@ -6,6 +6,20 @@ public class MahasiswaDemo {
         m1.alamat = "Nias, Sumatera Utara";
         m1.kelas = "2A";
 
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "023831";
+        m2.nama = "Rahma Putra";
+        m2.alamat = "Surabaya, Jawa Timur";
+        m2.kelas = "2C";
+
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "023999";
+        m3.nama = "Ayub Yori";
+        m3.alamat = "Madiun, Jawa Timur";
+        m3.kelas = "2E";
+
         m1.displayBiodata();
+        m2.displayBiodata();
+        m3.displayBiodata();
     }
 }

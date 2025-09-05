05f8131162098282f9de83fe54657d5c8f602052
Silmiuww <nadiasalma078@gmail.com>
2025-09-05 07:57:35 +0700

    Percobaan pertama

---
+++ Mahasiswa.java
@@ -0,0 +1,13 @@
+public class Mahasiswa {
+    public string nim;
+    public string nama;
+    public string alamat;
+    public string kelas;
+
+    public void displayBiodata() {
+        System.out.println("NIM      : " + nim);
+        System.out.println("Nama     : " + nama);
+        System.out.println("Alamat   : " + alamat);
+        System.out.println("kelas    : " + kelas);
+    }
+}
